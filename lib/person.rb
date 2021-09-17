5014e62d5b2065d2d58073667642e32436b085a0
thomas <thomasmonta98@gmail.com>
2021-09-17 20:08:17 +1000

    done with assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  def initialize(attributes)
+    attributes.each do |attribute, value|
+      self.class.attr_accessor(attribute)
+      self.send("#{attribute}=", value)
+    end
+  end
 end
